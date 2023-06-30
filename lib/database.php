1834d146dd54b9de1e928c3b7c0cefe58c6212c9
Josias Toribio <josias10.jtg@gmail.com>
2023-06-30 18:02:03 -0500

    Consultas

---
+++ lib/database.php
@@ -0,0 +1,58 @@
+<?php
+
+//se define la clase Database con cuatro propiedades
+class Database{
+    public $db; //variable publica que representa la conexión a la base de datos.
+
+    //se utilizan para almacenar resultados de consultas y preparar consultas
+    //son "protected" para no usarlas nuevamente
+    protected $resultado;
+    protected $prep;
+    protected $consulta;
+
+    public function __construct($dbhost, $dbuser, $dbpass, $dbname)
+    {
+        $this->db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+        if($this->db->connect_errno){
+            trigger_error("Fallo la conexion con MySQL, tipo de error -> ({$this->db->connect_error})", E_USER_ERROR);
+        }
+        $this->db->set_charset('utf8');
+    }
+    public function getUsuarios(){
+        $this->resultado = $this->db->query("SELECT * FROM usuarios");
+        return $this->resultado->fetch_all(); //Se cambia a all para obtener los resultados de la consulta
+    }
+
+
+    //Llamamos a consulta
+    public function preparar($consulta){
+        $this->consulta = $consulta;
+        $this->prep = $this->db->prepare($this->consulta);
+        if(!$this->prep){
+            trigger_error("Error al preparar la consulta",
+            E_USER_ERROR);
+        }
+    }
+
+    public function ejecutar(){
+        $this->prep->execute();
+    }
+    public function prep(){
+        return $this->prep;
+    }
+    public function resultado(){
+        return $this->prep->fetch();
+    }
+    public function cambiarDatabase($db){
+        $this->db->select_db($db);
+    }
+
+    //Validando
+    public function validarDatos($columna, $tabla, $condicion){
+        $this->resultado = $this->db->query("SELECT $columna FROM $tabla WHERE $columna = '$condicion'");
+        $chequear = $this -> resultado->num_rows;
+        return $chequear;
+
+    }
+}
+?>
--- registrar.php
+++ registrar.php
@@ -12,8 +12,14 @@
             </div>
         </div>
         
-        <div class="col-md-6 caja col-centrar">
+        <div class="col-md-12 caja col-centrar">
                 <?php
+                    //Para poder llamar a la db
+                    require_once 'lib/config.php';
+                    spl_autoload_register(function($clase){
+                        require_once "lib/$clase.php";
+                    });
+
                     if($_POST){
                         extract($_POST, EXTR_OVERWRITE);
                         
@@ -28,22 +34,70 @@
                         } //El nombre de la foto lo convierte en minúscula
                         
                     }
+                    
+                    //Conexion a la base de datos
+                    $db = new Database(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+                    /*$array = $db->getUsuarios();
+                    
+                    //------------Todo lo comentado es para probar
+                    echo "<table class ='table table-cell'>
+                            <thead>
+                                <tr>
+                                    <td>id</td>
+                                    <td>nombre</td>
+                                    <td>apellido</td>
+                                    <td>email</td>
+                                    <td>contraseña</td>
+                                    <td>dni</td>
+                                    <td>celular</td>
+                                    <td>direccion</td>
+                                    <td>edad</td>
+                                    <td>ciudad</td>
+                                    <td>departamento</td>
+                                    <td>codigo postal</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                    ";
+
+                    //var_dump($array);
+                    foreach($array as $v){
+                        echo "<tr>";
+                        foreach($v as $v2){ //para mostrar todos los datos 
+                            echo "<td>$v2</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    echo "</tbody>";
+                    echo "</table>"; */
+
+                    $db->preparar("SELECT nombre, apellido, email FROM usuarios");
+                    $db->ejecutar();
+                    $db->prep()->bind_result(/* $id, */ $nombre, $apellido, $email/* , $a, $b, $c, $d, $e, $f, $g, $h */);
+                    echo "<table class ='table table-cell'>
+                            <thead>
+                                <tr>
+                                    "/* <td>nombre</td>
+                                    <td>apellido</td>
+                                    <td>email</td> */,"
+                                </tr>
+                            </thead>
+                        <tbody>";
+                    while($db->resultado()){ //Imprime los datos con diferentes va
+                        echo "<tr>
+                                <td>$nombre</td>
+                                <td>$apellido</td>
+                                <td>$email</td>                         
+                            </tr>";
+                    }
+                    echo "</tbody>";
+                    echo "</table>";
+
+                    echo $db->validarDatos('apellido', 'usuarios', 'toribio');
                 ?>
 
-                <form action="" enctype="multipart/form-data" method="POST">
+                <!-- <form action="" enctype="multipart/form-data" method="POST">
                     <legend>Registrate</legend>
-
-                    <div class="form-group">
-                        <input name="usuario" type="text"
-                        class="form-control" id=""
-                        placeholder="Usuario">
-                    </div>
-                    
-                    <div class="form-group">
-                        <input name="contrasena" type="text"
-                        class="form-control" id=""
-                        placeholder="Contrasena">
-                    </div>
                     
                     <div class="form-group">
                         <input name="nombre" type="text"
@@ -55,9 +109,24 @@
                         <input name="apellido" type="text"
                         class="form-control" id=""
                         placeholder="Apellido">
+                    </div> -->
+
+
+                    <!-- /////////////AGREGAR EMAIL Y CONTRASEÑA/////////////// -->
+                    <!-- <div class="form-group">
+                        <input name="email" type="text"
+                        class="form-control" id=""
+                        placeholder="Email">
                     </div>
                     
                     <div class="form-group">
+                        <input name="contrasena" type="password"
+                        class="form-control" id=""
+                        placeholder="Contraseña">
+                    </div> -->
+                    <!-- //////////////////////////////// -->
+                    
+                    <!-- <div class="form-group">
                         <input name="dni" type="text"
                         class="form-control" id=""
                         placeholder="Dni">
@@ -108,7 +177,7 @@
                     <button type="submit" class="btn btn-success">Registrar</button>
                     <a class=pull-right
                     href="index.php">Click aqui si ya tienes una cuenta</a>
-                </form>
+                </form> -->
                 
                 
         </div>
